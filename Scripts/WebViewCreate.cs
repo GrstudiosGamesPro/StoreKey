50f5a677fe6a92cace9e7fc571c85707e63f99e3
Esteban Nova <85538374+GrstudiosGamesPro@users.noreply.github.com>
2023-10-16 18:56:46 -0500

    data new

--- Scripts/WebViewCreate.cs
+++ Scripts/WebViewCreate.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.Web.WebView2.WinForms;
+﻿using Microsoft.Web.WebView2.Core;
+using Microsoft.Web.WebView2.WinForms;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -11,16 +12,32 @@ namespace KeyStore.Scripts
     public class WebViewCreate
     {
         public WebView2 web = new WebView2();
+        string webPath = "Data/Web/";
+        string htmlIndex;
 
         public void RunWeb()
         {
+            htmlIndex = File.ReadAllText ("E:\\VISUAL STUDIO\\StoreKey\\bin\\Debug\\net6.0-windows\\Data\\Web\\HMTL/index.html");
+
+            Console.WriteLine ("Web content: " + htmlIndex);
+
             web.Dock = DockStyle.Fill;
-            web.Source = new Uri ("https://www.youtube.com/watch?v=7LhwRtwEDvw");
+            web.CoreWebView2InitializationCompleted += OnChargeWeb;
+            web.EnsureCoreWebView2Async (null);
         }
 
-        public void OnChargeWeb()
+        public void OnChargeWeb (object sender, CoreWebView2InitializationCompletedEventArgs e)
         {
-            Console.WriteLine();
+            if (e.IsSuccess)
+            {
+                web.NavigateToString(htmlIndex);
+                Console.WriteLine ("Web started");
+            }
+            else
+            {
+                MessageBox.Show ("ERROR WHEN CREATING APP WINDOW", "ERROR 3", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                Console.WriteLine ("Error on start web");
+            }
         }
     }
 }
